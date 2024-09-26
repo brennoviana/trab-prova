6f3e0319e818ab43888798da6034ab3b9db3d545
brennovianacoelho <brenno.viana@smartnx.com>
2024-09-26 18:34:30 -0300

    commit rooms

--- backend/src/app/socketHandler.js
+++ backend/src/app/socketHandler.js
@@ -1,25 +1,13 @@
 export const socketHandler = (io) => {
     io.on("connection", (socket) => {
       console.log("Novo socket conectado:", socket.id);
-  
-      socket.on('test', (data) => {
-        console.log('Evento de teste recebido:', data);
-      });
               
-      socket.on("join-room", (roomId) => {
-        console.log(`Socket ${socket.id} entrou na sala ${roomId}`);
-        socket.join(roomId);
+      socket.on("join-room", (event) => {
+        console.log(event.room_id);
+        console.log(`Socket ${socket.id} entrou na sala ${event.room_id}`);
+        socket.join(event.room_id);
   
-        socket.to(roomId).emit("user-joined", `Usuário ${socket.id} entrou na sala ${roomId}`);
-      });
-  
-      socket.on("send-message", ({ roomId, message }) => {
-        console.log(`Mensagem de ${socket.id} na sala ${roomId}: ${message}`);
-  
-        socket.to(roomId).emit("receive-message", {
-          message,
-          sender: socket.id,
-        });
+        socket.to(event.room_id).emit("user-joined", `Usuário ${socket.id} entrou na sala ${event.room_id}`);
       });
   
       socket.on("disconnect", () => {
