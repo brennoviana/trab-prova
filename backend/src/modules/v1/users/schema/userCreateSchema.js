fb4e50c2cba03ae5d32a42dd7ac2e3d682db8d37
brennovianacoelho <brenno.viana@smartnx.com>
2024-09-25 22:15:13 -0300

    primeiro commit

--- backend/src/modules/v1/users/schema/userCreateSchema.js
+++ backend/src/modules/v1/users/schema/userCreateSchema.js
@@ -1,9 +1,38 @@
-import Joi from "joi";
+import Joi from 'joi';
 
 const userCreateSchema = Joi.object({
-  name: Joi.string().max(100).required(),
-  email: Joi.string().max(255).required(),
-  password: Joi.string().min(8).max(100).required(),
+  name: Joi.string()
+    .min(6) 
+    .max(100)
+    .required()
+    .messages({
+      'string.base': 'Name must be a string',
+      'string.empty': 'Name is required',
+      'string.max': 'Name must not exceed 100 characters',
+      'any.required': 'Name is required',
+    }),
+  
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .max(100)
+    .required()
+    .messages({
+      'string.base': 'Email must be a string',
+      'string.email': 'Email must be a valid email address',
+      'string.empty': 'Email is required',
+      'string.max': 'Email must not exceed 100 characters',
+      'any.required': 'Email is required',
+    }),
+  
+  password: Joi.string()
+    .min(6)
+    .required()
+    .messages({
+      'string.base': 'Password must be a string',
+      'string.empty': 'Password is required',
+      'string.min': 'Password must be at least 6 characters long',
+      'any.required': 'Password is required',
+    }),
 });
 
 export { userCreateSchema };
--- backend/src/modules/v1/users/schema/userUpdateSchema.js
+++ backend/src/modules/v1/users/schema/userUpdateSchema.js
@@ -1,9 +1,29 @@
 import Joi from "joi";
 
 const userUpdateSchema = Joi.object({
-  name: Joi.string().max(100).optional(),
-  email: Joi.string().max(255).optional(),
-  password: Joi.string().min(8).max(100).optional(),
+  name: Joi.string()
+  .min(6)
+  .max(100)
+  .optional()
+  .messages({
+  'string.max': 'Name must not exceed 100 characters',
+  }),
+
+  email: Joi.string()
+  .email()
+  .max(255)
+  .optional()
+  .messages({
+    'string.email': 'Email must be a valid email address',
+  }),
+  password: Joi.string()
+  .min(8)
+  .max(100)
+  .optional()
+  .messages({
+    'string.min': 'Password must be at least 8 characters long',
+    'string.max': 'Password must not exceed 100 characters',
+  }),
 });
 
 export { userUpdateSchema };
