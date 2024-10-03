b3d7e27b1aacd12e82d6d68ea71f33a206c67453
brennovianacoelho <brenno.viana@smartnx.com>
2024-10-03 12:23:19 -0300

    commit swagger

--- backend/src/app/socketHandler.js
+++ backend/src/app/socketHandler.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export const socketHandler = (io) => {
     io.on("connection", (socket) => {
 
@@ -7,6 +9,13 @@ export const socketHandler = (io) => {
             "user-connected", 
             `Usuário ${event.userId} entrou na sala ${event.roomId}`
         );
+
+        try {
+          const response = axios.post(`http://localhost:3000/api/v1/rooms/${roomId}/join`);
+          console.log('Requisição bem-sucedida:', response.data);
+        } catch (error) {
+          console.error('Erro ao fazer a requisição:', error);
+        }
 
         socket.on('signal', (data) => {
           const { roomId, signalData } = data;
